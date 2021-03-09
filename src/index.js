45100b913c824120c185de13f6b2689ece2d980d
evgeny <bakalov.es@ya.ru>
2021-03-09 08:14:18 +0300

    init: finish task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n < 0) {
+        return reverse(-n);
+    }
+
+    return n.toString().split('').reverse().join('');
 }
